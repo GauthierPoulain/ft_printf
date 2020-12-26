33f7f4f7ab8df910ea8b2251f6328c7a4fba20a4
gapoulai <gapoulai@student.42lyon.fr>
2020-12-26 14:04:26 +0100

    end pointer

--- src/print_pointer.c
+++ src/print_pointer.c
@@ -6,22 +6,80 @@
 /*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 15:31:20 by gapoulai          #+#    #+#             */
-/*   Updated: 2020/12/22 15:55:41 by gapoulai         ###   ########lyon.fr   */
+/*   Updated: 2020/12/26 13:58:11 by gapoulai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-void		print_pointer(t_flags *flags, size_t adr)
+static	void	print_other(t_flags *flags, char *tmp)
+{
+	if (flags->minus)
+	{
+		flags->print += ft_putstr_fd(tmp, 1);
+		print_width(flags->width, ft_strlen(tmp), 0, flags);
+	}
+	else if (flags->zero && !(flags->has_dot))
+	{
+		print_width(flags->width, ft_strlen(tmp), 1, flags);
+		flags->print += ft_putstr_fd(tmp, 1);
+	}
+	else
+	{
+		print_width(flags->width, ft_strlen(tmp), 0, flags);
+		flags->print += ft_putstr_fd(tmp, 1);
+	}
+}
+
+static void		ft_minus(t_flags *flags, char *tmp, unsigned long nbr)
+{
+	if (flags->has_dot)
+	{
+		if (!(flags->dot) && nbr == 0)
+			print_width(flags->width, flags->dot, 0, flags);
+		else
+		{
+			print_width(flags->dot, ft_strlen(tmp), 1, flags);
+			flags->print += ft_putstr_fd(tmp, 1);
+			if ((int)ft_strlen(tmp) < flags->dot)
+				print_width(flags->width, flags->dot, 0, flags);
+			else
+				print_width(flags->width, ft_strlen(tmp), 0, flags);
+		}
+	}
+	else
+		print_other(flags, tmp);
+}
+
+static void		ft_not_minus(t_flags *flags, char *tmp, int nbr)
+{
+	if (!(flags->dot) && flags->has_dot && nbr == 0)
+		print_width(flags->width, flags->dot, 0, flags);
+	else if (flags->dot)
+	{
+		if ((int)ft_strlen(tmp) < flags->dot)
+			print_width(flags->width, flags->dot, 0, flags);
+		else
+			print_width(flags->width, ft_strlen(tmp), 0, flags);
+		print_width(flags->dot, ft_strlen(tmp), 1, flags);
+		flags->print += ft_putstr_fd(tmp, 1);
+	}
+	else
+		print_other(flags, tmp);
+}
+
+void			print_pointer(t_flags *flags, size_t adr)
 {
 	char	*tmp;
-	char	*tmp2;
+	char	*res;
 
-	tmp = ft_itoa(adr);
-	tmp2 = ft_convert_base(tmp, DECIMAL, BASE_HEXA);
+	tmp = ft_uitoa_base(adr, 16);
+	ft_tolowercase(tmp);
+	res = ft_strjoin("0x", tmp);
 	free(tmp);
-	tmp = ft_strjoin("0x", tmp2);
-	free(tmp2);
-	print_string(flags, tmp);
-	free(tmp);
+	if (flags->minus)
+		ft_minus(flags, res, adr);
+	else
+		ft_not_minus(flags, res, adr);
+	free(res);
 }
--- src/print_string.c
+++ src/print_string.c
@@ -6,13 +6,13 @@
 /*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 15:21:26 by gapoulai          #+#    #+#             */
-/*   Updated: 2020/12/22 15:29:54 by gapoulai         ###   ########lyon.fr   */
+/*   Updated: 2020/12/26 13:57:39 by gapoulai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-static void	str_print_dot(t_flags *flags, char * str)
+static void	str_print_dot(t_flags *flags, char *str)
 {
 	if (flags->minus)
 	{
@@ -40,7 +40,7 @@ void		print_string(t_flags *flags, char *str)
 		{
 			flags->print += ft_putstr_fd(str, 1);
 			print_width(flags->width, ft_strlen(str), 0, flags);
-		}		
+		}
 	}
 	else if (flags->dot >= 0 && flags->has_dot == 1)
 		str_print_dot(flags, str);
